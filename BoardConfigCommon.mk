34d13a2c13b7ce86e7ced924c2f19f957f11a1e2
roynatech2544 <whiteshell2544@naver.com>
2023-08-14 12:13:22 +0900

    sm6225-common: Remove leftover of power HAL

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -158,10 +158,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := bengal
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/tp_gesture"
-TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
-
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
