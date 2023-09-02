de09cd87951022749978fdb03aa450c571309a7c
roynatech2544 <whiteshell2544@naver.com>
2023-09-02 22:23:36 +0900

    sm6225-common: Build LOS Livedisplay HAL
    Revert "sm6225-common: Remove unused Los FCM"
    
    This reverts commit 144d751f841888a63418df27dd6ecb1b7bc0e122.

--- bengal.mk
+++ bengal.mk
@@ -189,7 +189,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service
+    android.hardware.memtrack@1.0-service \
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 PRODUCT_PACKAGES += \
     memtrack.bengal \
