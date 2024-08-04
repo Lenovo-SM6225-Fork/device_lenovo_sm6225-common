02cf29d3800f276f011e9c16e85cbdb2ca70dbcb
roynatech2544 <whiteshell2544@naver.com>
2024-08-04 11:17:38 +0900

    sm6225-common: Update from ZUI_15.0.348_ST_230911

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -190,7 +190,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 ENABLE_VENDOR_RIL_SERVICE := false
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-11-01
+VENDOR_SECURITY_PATCH := 2023-08-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
